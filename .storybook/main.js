188ea4f494367bb66107f29ed99186ebfdca1228
Pablo Robert Pereira Alves <pablorobert@gmail.com>
2025-04-17 11:29:43 -0300

    chore: migrate Storybook to version 5

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+export const stories = ["../stories/*.tsx"];
+
+export const framework = {
+  name: "@storybook/react",
+  options: {},
+};
+
+export const docs = {
+  autodocs: true,
+};
+
+export const staticDirs = ["../docs"];
